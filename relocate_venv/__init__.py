2541eff8546bd5633216d4be236d0d9aa031539b
Nick Johnson <nick.johnson@ed.ac.uk>
2017-03-16 11:22:08 +0000

    Minor refactor of mapping code; Use more robust path modifications for ARCHER.

--- relocate_venv/__init__.py
+++ relocate_venv/__init__.py
@@ -1,7 +1,5 @@
-# License goes here ...
-import sys
-import os
 from .core import handle_args
+
 
 def entrypt():
     """Entry point for the application script"""
--- relocate_venv/core.py
+++ relocate_venv/core.py
@@ -4,6 +4,8 @@ import subprocess
 import sys
 import shutil
 import argparse
+import glob
+
 
 class UserError(Exception):
     pass
@@ -20,32 +22,16 @@ def grep_and_sed(src_dir, cur_dir, dst_dir):
 
 def main(src_dir, new_dir, map_d, archer):
     """ Call out the necessary functions."""
-    
+
     print "Creating a copy of %s in %s" % (src_dir, new_dir)
-    # shutil.copytree(src_dir, cur_dir, symlinks=False, ignore=shutil.ignore_patterns('*.pyc'))
+    # shutil.copytree(src_dir, new_dir, symlinks=False, ignore=shutil.ignore_patterns('*.pyc'))
 
     for key in map_d.keys():
         grep_and_sed(key, new_dir, map_d[key])
 
-    # # If we are on ARCHER, we need to recall that /work/foo is really mounted at /fsX/foo
-    # # Using os.path always gives us the /fsX/foo representation, but install scripts sometimes dont resolve
-    # # paths. Similarly, /home/bar is really mounted at /homeX/bar.
-    # # So, work around this by calling grep_and_sed again with the extra patterns.
-    # if archer:
-    #     # Firstly, modify the path to look for /fsX and replace that part with /work
-    #     src_split = src_dir.split("/")
-    #     if src_split[1].startswith("fs"):
-    #         src_split[1] = "work"
-    #         src_split = '/'.join(src_split)
-    #     # Secondly, modify the path to look for /homeX and replace that part with /home
-    #     if src_split[1].startswith("home"):
-    #         src_split[1] = "home"
-    #         src_split = '/'.join(src_split)
-
-    #     grep_and_sed(src_split, cur_dir, dst_dir)
-
     print "Now ready to compress %s for easy moving.\n" % new_dir
     print "Ie: cd %s && tar -c -f ../firedrake.tar ." % new_dir
+
 
 def handle_args():
     parser = argparse.ArgumentParser()
@@ -53,33 +39,37 @@ def handle_args():
     parser.add_argument('--output-location', action='store', help='Path to a location where the new, modified virtualenv will be written.')
     parser.add_argument('--archer', '-a', help='If specified, perform ARCHER specific actions (path resolution etc.).', action='store_true')
     parser.add_argument('--map', '-m', nargs='+', action='append', help='provide a mapping')
-
     args = parser.parse_args()
 
+    # Existing virtualenv to pack up.
     existing = os.path.realpath(args.existing)
 
+    # If user specifies a location for the copy, use that, else default to ./firetemp
     if args.output_location:
         outputloc = os.path.realpath(args.output_location)
     else:
         outputloc = os.path.realpath(os.getcwd() + '/firetemp')
         print outputloc
 
-
     # If the user specified a mapping, use that, but sanitise the paths
     if args.map:
         _mapping = dict(args.map)
-        mapping_dict={}
+        mapping_dict = {}
         for key in _mapping.keys():
             mapping_dict[os.path.realpath(key)] = os.path.realpath(_mapping[key])
     else:
-        mapping_dict=dict([(existing, outputloc)])
+        mapping_dict = dict([(existing, outputloc)])
 
-    # If on ARCHER, do ARCHER path mods
+    # If user says they are using ARCHER, do ARCHER path mods
+    # These are necessary because not every Python module sanitises and realises paths.
     archer = False
     if args.archer:
-        print "ARCHER requested"
         archer = True
-
+        _mapping = mapping_dict
+        for key in _mapping.keys():
+            mapping_dict[glob.re.sub("/fs\d/", "/work/", key)] = mapping_dict[key]
+            mapping_dict[glob.re.sub("/home\d/", "/home/", key)] = mapping_dict[key]
+        
 
     # Diagnostic
     print "\nMapping Table"
@@ -90,17 +80,15 @@ def handle_args():
 
     # existing is where the target venv was installed to originally
     # outputloc is where the target venv will be created
-    # newloc is where the paths in the target venv will be pointed to, for example /tmp/firedrake
+    # mapping_dict is a dict containing the mapping between existing paths, and what they
+    # should become.
     try:
         main(existing, outputloc, mapping_dict, archer)
     except UserError:
         e = sys.exc_info()[1]
         parser.error(str(e))
-    
-
 
 
 # Provide mechanism to run this as a script if required
 if __name__ == '__main__':
     handle_args()
-    
